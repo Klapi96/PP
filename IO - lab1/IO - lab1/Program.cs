68ad9ee59c7b65517d7107914ba6f2c58a6ff04b
Klapi96 <klapi96@onet.pl>
2017-11-29 00:53:01 +0100

    Add lab1

---
+++ IO - lab1/IO - lab1/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace IO___lab1
+{
+    class Program
+    {
+        private static object lockZad4 = new object();
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello world");
+            //--------------------------------------------------------------ZAD 1
+            //ThreadPool.QueueUserWorkItem(ThreadProc, new object[] { 500 });
+            //ThreadPool.QueueUserWorkItem(ThreadProc, new object[] { 1500 });
+            //Thread.Sleep(4000);
+            //--------------------------------------------------------------END ZAD 1
+
+            //--------------------------------------------------------------ZAD 2,3,4
+            ThreadPool.QueueUserWorkItem(serverr);
+            ThreadPool.QueueUserWorkItem(client1);
+            ThreadPool.QueueUserWorkItem(client2);
+            Thread.Sleep(4000);
+            //---------------------------------------------------------------END ZAD 2,3,4
+        }
+
+        //------------------------------------------ZAD 1
+        static void ThreadProc(Object stateInfo)
+        {
+            Thread.Sleep((int)((object[])stateInfo)[0]);
+            Console.WriteLine("ELO");
+        }
+        //------------------------------------------END ZAD 1
+
+
+        //------------------------------------------ZAD 2,3,4
+
+        static void writeConsoleMessage(string message, ConsoleColor color)
+        {
+            lock (lockZad4)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+            
+        }
+
+        static void serverr(Object stateInfo)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 2048);
+            server.Start();
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                if (client != null)
+                {
+                    ThreadPool.QueueUserWorkItem(ThreadProcServerHandler, new object[] { client });
+                }
+                
+            }
+        }
+
+        static void ThreadProcServerHandler(Object stateInfo)
+        {
+            
+            byte[] buffer = new byte[1024];
+            ((TcpClient)((object[])stateInfo)[0]).GetStream().Read(buffer, 0, 1024);
+            string wiadd = ASCIIEncoding.ASCII.GetString(buffer);
+            writeConsoleMessage(wiadd, ConsoleColor.Red);
+            
+            ((TcpClient)((object[])stateInfo)[0]).GetStream().Write(buffer, 0, buffer.Length);
+
+        }
+
+        static void client1(Object stateInfo)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2048));
+            NetworkStream stream = client.GetStream();
+            byte[] message = new byte[1024];
+            String wiad = "wiadomosc-1";
+            for (int i = 0; i < wiad.Length; i++)
+            {
+                message[i] = (byte)wiad[i];
+            }
+            stream.Write(message, 0, message.Length);
+    
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+
+                client.GetStream().Read(buffer, 0, buffer.Length);
+                string wiadd = ASCIIEncoding.ASCII.GetString(buffer);
+                wiadd = "otrzymalem wiadomosc:" + wiadd;
+                writeConsoleMessage(wiadd, ConsoleColor.Green);
+            }
+        }
+        static void client2(Object stateInfo)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2048));
+            NetworkStream stream = client.GetStream();
+            byte[] message = new byte[1024];
+            String wiad = "wiadomosc-2";
+            for (int i = 0; i < wiad.Length; i++)
+            {
+                message[i] = (byte)wiad[i];
+            }
+            stream.Write(message, 0, message.Length);
+
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+
+                client.GetStream().Read(buffer, 0, buffer.Length);
+                string wiadd = ASCIIEncoding.ASCII.GetString(buffer);
+                wiadd = "otrzymalem wiadomosc:" + wiadd;
+                writeConsoleMessage(wiadd, ConsoleColor.Green);
+            }
+        }
+        //------------------------------------------END ZAD 2,3,4
+    }
+}
---
+++ IO - lab1/lab1zad5/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace lab1zad5
+{
+    class Program
+    {
+        public static int suma;
+        public static int[] tab;
+        private static void zrobTabele(int[] x)
+        {
+            Random R = new Random();
+            for(int i=0; i<x.Length;i++)
+            {
+                x[i] = 1;
+                //x[i] = R.Next(10);
+            }
+        }
+        static void Main(string[] args)
+        {
+            suma = 0;
+            Console.WriteLine("Jak duza ma byc tablica?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            tab = new int[n];
+            zrobTabele(tab);
+
+            Console.WriteLine("Jakiej wielkosci ma byc fragment?");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int numberOfFragments=0;
+            if (x > n)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                if (n % x == 0)
+                {
+                    numberOfFragments = n / x;
+                }
+                else
+                {
+                    numberOfFragments = (n / x) + 1;
+                }
+            }
+
+            WaitHandle[] waitHandles = new WaitHandle[numberOfFragments];
+            for(int k = 0; k < waitHandles.Length; k++)
+            {
+                waitHandles[k] = new AutoResetEvent(false);
+            }
+
+            int start = 0;
+            for(int j = 0;j < numberOfFragments; j++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc), new object[] { start, ((j+1)* x)-1, waitHandles[j] });
+                start += x;
+            }
+
+            WaitHandle.WaitAll(waitHandles);
+            Console.WriteLine("Suma globalna " + suma);
+        }
+
+        static void ThreadProc(Object stateInfo)
+        {
+            int x = ((int)((object[])stateInfo)[0]);
+            int y = ((int)((object[])stateInfo)[1]);
+            int sumka = 0;
+
+            for (int i = x; i <= y; i++)
+            {
+                if (i >= tab.Length)
+                {
+                    break;
+                }
+                else
+                {
+                    sumka += tab[i];
+                }
+                
+            }
+            Console.WriteLine("Suma lokalna "+sumka);
+            suma += sumka;
+            AutoResetEvent waitHandle = (AutoResetEvent)((object[])stateInfo)[2];
+            waitHandle.Set();
+        }
+    }
+}
